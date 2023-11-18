3cf90047cb16b1652a52f3f2f92bdf44159abad7
Cauabc <cauacardosoyt@gmail.com>
2023-11-18 13:07:40 -0300

    Front end Done

--- ListaDeTarefas.Front/README.md
+++ ListaDeTarefas.Front/README.md
@@ -1,8 +1,22 @@
-# React + Vite
+# **Lista de Tarefas**
+<<<<<<< HEAD
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+### Instalação
+Para rodar o Front-End, abra o **ListaDeTarefas.FRONT**, execute o comando **npm install**, após, execute o comando **npm run dev**.
+> **Pronto, lembre-se de rodar o back-end primeiro!**
+=======
+Esse é um projeto FullStack que desenvolvi para treinar e adquirir experiência, usando C# WebAPI e ReactJS
+#### Nesse projeto você poderá:
+- Criar tarefas
+- Deletar tarefas
+- Todas as tarefas permanecerão lá mesmo após fechar, estarão armazenadas em um banco de dados
+- Filtrar tarefas por nome
 
-Currently, two official plugins are available:
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+### Configuração
+Para rodar o projeto, abra o ListaDeTarefas.API, entre novamente na pasta **ListaDeTarefas.API** , execute o comando **dotnet watch run**, após abra o **ListaDeTarefas.FRONT**, execute o comando **npm install**, após, execute o comando **npm run dev**.
+> **Pronto, tudo deve funcionar normalmente**
+>
+> ![image](https://github.com/Cauabc/ListaDeTarefas-FullStack/assets/115277406/ad8cf459-f9c8-422d-bf52-a3a696595a83)
+
+>>>>>>> c837d20953b82b6ee6964befb797a839f4fe9144
--- ListaDeTarefas.Front/src/App.jsx
+++ ListaDeTarefas.Front/src/App.jsx
@@ -1,35 +1,266 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { MdDone } from "react-icons/md";
+import { FaTrashAlt } from "react-icons/fa";
+import { useEffect, useState, useRef } from "react";
+import { IoSearchOutline } from "react-icons/io5";
+import axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState([]);
+  const [taskName, setTaskName] = useState("");
+  const [filteredData, setFilteredData] = useState([]);
+  const [taskFilterValue, setTaskFilterValue] = useState("");
+  const [isDisabled, setIsDisabled] = useState(true);
+  const filterSpanRef = useRef(null);
+  const filterInputRef = useRef(null);
+  const filterContainerRef = useRef(null);
+  const urlAPI = "https://localhost:7276/api/Task";
+  const notify = () =>
+    toast.success("Tarefa adicionada!", {
+      position: "top-right",
+      autoClose: 1500,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: false,
+      draggable: true,
+      progress: undefined,
+      theme: "dark",
+    });
+  const notifyError = () =>
+    toast.error("Tarefa já existente!", {
+      position: "top-right",
+      autoClose: 1500,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: false,
+      draggable: true,
+      progress: undefined,
+      theme: "dark",
+    });
+  const notifyDelete = () =>
+    toast.success("Tarefa removida!", {
+      position: "top-right",
+      autoClose: 1500,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: false,
+      draggable: true,
+      progress: undefined,
+      theme: "dark",
+    });
+
+  useEffect(() => {
+    async function getData() {
+      try {
+        const response = await axios.get(urlAPI);
+        setData(response.data);
+      } catch (error) {
+        console.error("Erro ao buscar tarefas: ", error);
+      }
+    }
+
+    getData();
+  }, []);
+
+  useEffect(() => {
+    taskName.length > 0 ? setIsDisabled(false) : setIsDisabled(true);
+  }, [taskName]);
+
+  const handleKeyDown = (e) => {
+    if (e.keyCode === 13) {
+      handleAddTask();
+    }
+  };
+
+  const handleAddTask = async () => {
+    try {
+      await axios.post(`${urlAPI}`, {
+        name: taskName,
+        isCompleted: false,
+      });
+
+      const response = await axios.get(urlAPI);
+      setData(response.data);
+
+      setTaskName("");
+    } catch (error) {
+      notifyError();
+      return console.log("Error while adding item:", error);
+    }
+    notify();
+  };
+
+  const handleDeleteItem = async (id) => {
+    try {
+      await axios.delete(`${urlAPI}/${id}`);
+      const updatedTasks = await axios.get(urlAPI);
+      setData(updatedTasks.data);
+    } catch (error) {
+      return console.error("Error trying to delete item:", error);
+    }
+    notifyDelete();
+  };
+
+  const handleChangeCompleted = async (id) => {
+    const taskToUpdate = data.find((item) => item.id === id);
+
+    if (!taskToUpdate) return;
+
+    const updatedTask = {
+      ...taskToUpdate,
+      isCompleted: !taskToUpdate.isCompleted,
+    };
+
+    try {
+      await axios.put(`${urlAPI}/${id}`, updatedTask);
+      const response = await axios.get(urlAPI);
+      setData(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleEnableFilter = () => {
+    filterSpanRef.current.classList.add("disabled");
+    filterInputRef.current.classList.remove("disabled");
+    filterContainerRef.current.classList.add("wide");
+  };
+
+  const handleGenerateExcel = async () => {
+    try {
+      const response = await axios.get(`${urlAPI}/gerarExcel`, {
+        responseType: "blob",
+      });
+
+      const url = window.URL.createObjectURL(new Blob([response.data]));
+
+      const link = document.createElement("a");
+      link.href = url;
+      link.setAttribute("download", "tarefas.xlsx");
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+    } catch (error) {
+      console.error("Erro ao gerar o arquivo Excel:", error);
+    }
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <ToastContainer />
+      <main className="mainContent">
+        <div className="header">
+          <h1>lista de tarefas</h1>
+        </div>
+        <div className="inputArea">
+          <input
+            onKeyDown={(e) => handleKeyDown(e)}
+            type="text"
+            name="inputTask"
+            id="inputTask"
+            placeholder="Nome da tarefa"
+            value={taskName}
+            onChange={(e) => setTaskName(e.target.value)}
+          />
+          <button onClick={() => handleAddTask()} disabled={isDisabled}>
+            Adicionar
+          </button>
+        </div>
+        <div className="filterTask" ref={filterContainerRef}>
+          <div className="filterIcon" onClick={() => handleEnableFilter()}>
+            <span ref={filterSpanRef}>Filtrar</span>
+            <input
+              ref={filterInputRef}
+              value={taskFilterValue}
+              onChange={(e) => {
+                setTaskFilterValue(e.target.value);
+                setFilteredData(
+                  data.filter((item) => item.name.includes(e.target.value))
+                );
+              }}
+              type="text"
+              name="filterTask"
+              id="filterTask"
+              placeholder="Filtrar tarefa"
+              className="disabled"
+            />
+            <IoSearchOutline />
+          </div>
+        </div>
+        {data.length > 0 && (
+          <div className="taskStatus">
+            <p>
+              Total: <span>{data.length}</span>
+            </p>
+            <p>
+              Completas:{" "}
+              <span>{data.filter((x) => x.isCompleted == true).length}</span>
+            </p>
+          </div>
+        )}
+        {taskFilterValue
+          ? filteredData.map((itemFiltered) => (
+              <div key={itemFiltered.id} className="taskComponent">
+                <div className="taskName">
+                  <input
+                    type="checkbox"
+                    name={itemFiltered.name}
+                    id={itemFiltered.name}
+                    onChange={() => {
+                      handleChangeCompleted(itemFiltered.id);
+                      itemFiltered.isCompleted = !itemFiltered.isCompleted;
+                    }}
+                    checked={itemFiltered.isCompleted}
+                  />
+                  <MdDone className="taskNameIcon" />
+                  <label htmlFor={itemFiltered.name}>{itemFiltered.name}</label>
+                </div>
+                <div className="taskDelete">
+                  <button
+                    onClick={() => {
+                      handleDeleteItem(itemFiltered.id);
+                      setFilteredData(
+                        data.filter((dataItem) =>
+                          dataItem.name.includes(
+                            taskFilterValue != itemFiltered.name
+                          )
+                        )
+                      );
+                    }}
+                  >
+                    <FaTrashAlt className="taskDeleteIcon" />
+                  </button>
+                </div>
+              </div>
+            ))
+          : data.map((item) => (
+              <div key={item.id} className="taskComponent">
+                <div className="taskName">
+                  <input
+                    type="checkbox"
+                    name={item.name}
+                    id={item.name}
+                    onChange={() => handleChangeCompleted(item.id)}
+                    checked={item.isCompleted}
+                  />
+                  <MdDone className="taskNameIcon" />
+                  <label htmlFor={item.name}>{item.name}</label>
+                </div>
+                <div className="taskDelete">
+                  <button onClick={() => handleDeleteItem(item.id)}>
+                    <FaTrashAlt className="taskDeleteIcon" />
+                  </button>
+                </div>
+              </div>
+            ))}
+      </main>
+      <div className="exportExcel">
+        <button onClick={() => handleGenerateExcel()}>Gerar Planilha</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
